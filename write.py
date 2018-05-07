43c1ec867d835df466e529027abf44c931dc0ebf
masato <masato@M-mbp.local>
2018-05-07 14:44:05 +0800

    something

--- write.py
+++ write.py
@@ -1,31 +1,49 @@
 poem = '''There was a young lady named Bright,
-    Whose speed was far faster than light;
-    She started one day
-    In a relative way,
-    And returned on the previous night.'''
+Whose speed was far faster than light;
+She started one day
+In a relative way,
+And returned on the previous night.'''
 # print(len(poem))
 #
 # fout = open('relative', 'wt')
 # fout.write(poem)
 # fout.close()
 
-test_tx = '''testです。'''
+# test_tx = '''testです。'''
+#
+# fout = open('test_tx.txt', 'wt')
+# print(test_tx, file=fout)
+# fout.close()
+#
+# fout = open('relativity2', 'wt')
+# print(poem)
+# print(poem, file=fout)
+# fout.close()
+#
+# fout = open('relativity', 'wt')
+# size = len(poem)
+# offset = 0
+# chunk = 100
+# while True:
+#     if offset > size:
+#         break
+#     fout.write(poem[offset:offset+chunk])
+#     offset += chunk
 
-fout = open('test_tx.txt', 'wt')
-print(test_tx, file=fout)
-fout.close()
-
-fout = open('relativity2', 'wt')
-print(poem)
-print(poem, file=fout)
-fout.close()
+# fout = open('relativity', 'wt')
+# print(poem, file=fout, sep='', end='')
+# fout.close()
 
 fout = open('relativity', 'wt')
 size = len(poem)
+print(size)
 offset = 0
 chunk = 100
 while True:
+    print(offset)
     if offset > size:
         break
     fout.write(poem[offset:offset+chunk])
     offset += chunk
+
+fout.close()
